c753f3d29e3caffb51036cf181a132fa7f714acd
Akshay9522-cell <akshaynamdev963@gmail.com>
2025-03-25 09:15:25 +0530

    second

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 
  //const URL='http://localhost:8000/bank'
-const URL='https://placement-project-1-2.onrender.com/bank'
+const URL='https://e-bank-backend.onrender.com/bank'
 //const URL='https://placement-project-1-5.onrender.com/bank'
 
 
